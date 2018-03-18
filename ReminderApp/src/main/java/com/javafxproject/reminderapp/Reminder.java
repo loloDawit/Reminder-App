b3377a34001f27a60bbff7d619fa0bcca87782d3
Dawit Abera <dawitabera64@gmail.com>
2018-03-18 01:06:01 -0700

    Class is UPDATED  - class has an override method to make sure if two reminders are equal

--- ReminderApp/src/main/java/com/javafxproject/reminderapp/Reminder.java
+++ ReminderApp/src/main/java/com/javafxproject/reminderapp/Reminder.java
@@ -6,6 +6,8 @@ package com.javafxproject.reminderapp;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
+import java.util.Objects;
 import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
@@ -15,6 +17,7 @@ import javafx.beans.property.SimpleStringProperty;
  * @author dawitabera
  */
 class Reminder {
+    
     private SimpleStringProperty name;
     private SimpleStringProperty notes;
     private SimpleObjectProperty<LocalDate> date;
@@ -31,52 +34,102 @@ class Reminder {
 	completed = new SimpleBooleanProperty(false);
     }
 
-    public SimpleStringProperty getName() {
+    public String getName() {
+        return name.get();
+    }
+    
+    public SimpleStringProperty namProperty(){
         return name;
     }
 
-    public void setName(SimpleStringProperty name) {
-        this.name = name;
+    public void setName(String n) {
+        name.set(n);
     }
 
-    public SimpleStringProperty getNotes() {
-        return notes;
+    public String getNotes() {
+        return notes.get();
     }
 
-    public void setNotes(SimpleStringProperty notes) {
-        this.notes = notes;
+    public void setNotes(String n) {
+        notes.set(n);
     }
-
-    public SimpleObjectProperty<LocalDate> getDate() {
+    public LocalDate getDate(){
+        return date.get();
+    }
+    public SimpleObjectProperty<LocalDate> dateProperty() {
         return date;
     }
 
-    public void setDate(SimpleObjectProperty<LocalDate> date) {
-        this.date = date;
+    public void setDate(LocalDate d) {
+        date.set(d);
     }
-
-    public SimpleObjectProperty<LocalTime> getTime() {
+    public LocalTime gettTime(){
+        return time.get();
+    }
+    public SimpleObjectProperty<LocalTime> timeProperty() {
         return time;
     }
 
-    public void setTime(SimpleObjectProperty<LocalTime> time) {
-        this.time = time;
+    public void setTime(LocalTime t) {
+        // time is stored in min
+        time.set(t.truncatedTo(ChronoUnit.MINUTES));
     }
 
-    public SimpleBooleanProperty getPriority() {
-        return priority;
+    public boolean getPriority() {
+        return priority.get();
     }
 
-    public void setPriority(SimpleBooleanProperty priority) {
-        this.priority = priority;
+    public void setPriority(boolean p) {
+        priority.set(p);
     }
 
-    public SimpleBooleanProperty getCompleted() {
-        return completed;
+    public boolean getCompleted() {
+        return completed.get();
     }
 
-    public void setCompleted(SimpleBooleanProperty completed) {
-        this.completed = completed;
+    public void setCompleted(boolean complete) {
+        completed.set(complete);
+    }
+
+    @Override
+    public String toString() {
+        return name.toString(); 
+        
+    }
+    /**
+     * Reminders are equal when their names are same on the same date.
+     * @param obj to be compared to
+     * @return boolean depending on the logic 
+     * NOTE: 
+     *      equality operator (==) compares the references (addresses in memory) 
+     *      of the two Strings as two different numbers - this is known as 
+     *      reference equality. In this case both String objects COULD have 
+     *      the exact same value, but they are in different physical memory 
+     *      locations so their references (addresses in memory) are different 
+     *      and the result is false.
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if(obj instanceof Reminder){
+            Reminder reminder = (Reminder)obj;
+            if ((reminder.getName().equals(name.get())) && (reminder.getDate().
+                    isEqual(date.get()))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 59 * hash + Objects.hashCode(this.name);
+        hash = 59 * hash + Objects.hashCode(this.notes);
+        hash = 59 * hash + Objects.hashCode(this.date);
+        hash = 59 * hash + Objects.hashCode(this.time);
+        hash = 59 * hash + Objects.hashCode(this.priority);
+        hash = 59 * hash + Objects.hashCode(this.completed);
+        return hash;
     }
     
     
