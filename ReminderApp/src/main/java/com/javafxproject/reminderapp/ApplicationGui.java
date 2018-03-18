99f98ee3071b416ff9197f9eded8920b67de67b6
Dawit Abera <dawitabera64@gmail.com>
2018-03-17 23:51:51 -0700

    - class is defined - class builds and GUI

--- ReminderApp/src/main/java/com/javafxproject/reminderapp/ApplicationGui.java
+++ ReminderApp/src/main/java/com/javafxproject/reminderapp/ApplicationGui.java
@@ -1,7 +1,9 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Class creates the graphical user interface(GUI) for the application.
+ * Gets the application data from the database and shows the data in the reminder
+ * table. 
+ * Starts a timer and and schedule its reminder notification task
+ * Has a function to create, add, update or delete reminders
  */
 package com.javafxproject.reminderapp;
 
@@ -10,5 +12,15 @@ package com.javafxproject.reminderapp;
  * @author dawitabera
  */
 class ApplicationGui {
+    
+    /**
+     * Constructor
+     * @param dataAccess from the database
+     * @param checkRemindersTask check reminder task
+     * @param reminderDialog show current reminder 
+     */
+    ApplicationGui(DataAccess dataAccess, CheckRemindersTask checkRemindersTask, ReminderDialog reminderDialog) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
