bb797dfb243b1193e09646bdf3a70b3395d90779
Dawit Abera <dawitabera64@gmail.com>
2018-03-19 00:23:06 -0700

    - Added more jdbc template based methods

--- ReminderApp/src/main/java/com/javafxproject/reminderapp/DatabaseJdbcAccess.java
+++ ReminderApp/src/main/java/com/javafxproject/reminderapp/DatabaseJdbcAccess.java
@@ -4,8 +4,13 @@
  * 
  */
 package com.javafxproject.reminderapp;
+import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Time;
 import java.util.List;
 import  org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 /**
  *
  * @author dawitabera
@@ -29,7 +34,53 @@ class DatabaseJdbcAccess {
      * @return a list of reminders
      */
     public List<Reminder> getAllReminders(){
-        
+        return jdbcTemplate.query(GET_ALL_REMINDERS, new RowMapper<Reminder>(){
+            @Override
+            public Reminder mapRow(ResultSet rs, int rowNum) throws SQLException {
+                // create reminder object
+                Reminder reminder = new Reminder();
+                reminder.setName(rs.getString("name"));
+                reminder.setNotes(rs.getString("notes"));
+                reminder.setDate(rs.getDate("date").toLocalDate());
+                reminder.setTime(rs.getTime("time").toLocalTime());
+                reminder.setPriority(rs.getBoolean("priority"));
+                reminder.setCompleted(rs.getBoolean("completed"));
+                return reminder;
+            }
+        });
+    }
+    /**
+     * Update existing reminder
+     * @param oldReminder reminder to be  updated 
+     * @param newReminder newly created reminder 
+     */
+    public void updateReminder(Reminder oldReminder, Reminder newReminder){
+        jdbcTemplate.update(UPDATE_REMINDER, 
+                newReminder.getName(), 
+                newReminder.getNotes(),
+                Date.valueOf(newReminder.getDate()),
+                Time.valueOf(newReminder.gettTime()),
+                newReminder.getPriority(),
+                newReminder.getCompleted(),
+                oldReminder.getName(),
+                Date.valueOf(oldReminder.getDate())
+        );
+    }
+    /**
+     * 
+     * @param reminderToBeDeleted 
+     */
+    public void deleteReminder(Reminder reminderToBeDeleted){
+        jdbcTemplate.update(DELETE_REMINDER, 
+                reminderToBeDeleted.getName(),
+                Date.valueOf(reminderToBeDeleted.getDate())
+        );
+    }
+    /**
+     * shutdown database connection 
+     */
+    public void killDatabase(){
+        jdbcTemplate.execute("SHUTDOWN");
     }
         
 }
