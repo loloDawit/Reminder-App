9e41d9b2f7b7b3d2fd1b4f7320e7aeb1f9cec0fa
Dawit Abera <dawitabera64@gmail.com>
2018-03-19 08:07:34 -0700

    - class has method that return custom date and time to the table cell

---
+++ ReminderApp/src/main/java/com/javafxproject/reminderapp/TableViewRowAndCellFactories.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.javafxproject.reminderapp;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import javafx.geometry.Pos;
+import javafx.scene.control.TableCell;
+
+/**
+ *
+ * @author dawitabera
+ */
+public class TableViewRowAndCellFactories {
+
+    /**
+     *
+     * @return table cell with custom date formating 
+     */
+    public TableCell<Reminder, LocalDate> getTableCellWithDateFormatting() {
+        TableCell<Reminder, LocalDate> cell = new TableCell<Reminder,LocalDate>(){
+           @Override
+           protected void updateItem(LocalDate date, boolean empty){
+               super.updateItem(date, empty);
+               if(date == null || empty){
+                   setText(null);
+               }else{
+                   setText(date.format(DateTimeFormatter.ofPattern("dd.MMM.yyyy")));
+               }
+           }
+        };
+        cell.setAlignment(Pos.CENTER);
+        return cell;
+    }
+    /**
+     * 
+     * @return Table cell with custom time formating 
+     */
+    public TableCell<Reminder, LocalTime> getTableCellWithFormating(){
+        TableCell<Reminder, LocalTime> cell = new TableCell<Reminder,LocalTime>(){
+            @Override
+            protected void updateItem(LocalTime time, boolean empty){
+                super.updateItem(time, empty);
+                if(time == null || empty){
+                    setText(null);
+                }else{
+                    setText(time.format(DateTimeFormatter.ofPattern("HH:mm")));
+                }
+            }
+        };
+        cell.setAlignment(Pos.CENTER);
+        return cell;
+    }
+    
+}
